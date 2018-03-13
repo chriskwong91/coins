a3625344636ab5c66ec06cbad41e99e7a36db8e8
christopher <christopher@yahoo-inc.com>
2018-03-12 17:56:49 -0700

    Use promises

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const request = require('request');
+const request = require('request-promise');
 const chalk = require('chalk');
 const numeral = require('numeral');
 
 const URLS = [
-  'https://api.coinmarketcap.com/v1/ticker/vechain/',
+  'https://api.coinmarketcap.com/v1/global/',
   'https://api.coinmarketcap.com/v1/ticker/bitcoin/',
+  'https://api.coinmarketcap.com/v1/ticker/vechain/',
 ];
 
 function formatDate(date) {
@@ -18,45 +19,41 @@ function formatDate(date) {
   return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
 }
 
-function makeRequest(url, params, cb) {
-  request(url, function(err, res) {
-    if (err) {
-      console.log('Error fetching the coin', err);
-      return;
-    }
-
-  
-    let data = res.body; 
-
+function makeRequest(url) {
+  return request(url).then(res => {
+    let data = res; 
     try {
-      data = eval(res.body)[0];
+      if (url.includes('global')) {
+        data = JSON.parse(res);
+        const {total_market_cap_usd, total_24h_volume_usd, bitcoin_percentage_of_market_cap} = data;
+        data = `MC: ${numeral(total_market_cap_usd).format('($ 0.00 a)')}   Vol: ${numeral(total_24h_volume_usd).format('($ 0.00a)')}   Ratio: ${(total_24h_volume_usd/total_market_cap_usd).toFixed(3)}   BTC Dominance: ${bitcoin_percentage_of_market_cap}%`;
+
+      } else {
+        data = eval(data)[0];
+        const {name, symbol, rank, price_usd, percent_change_24h } = data;
+        const pc = numeral(percent_change_24h).value();
+        const color = pc < 0 ? 'red' : 'green';
+        const pc_text = chalk[color](pc + '%');
+        const price = chalk[color](`$${price_usd}`);
+        data = `#${rank}\t${name}(${symbol})\t${price} \t${pc_text}`;
+      }
     } catch(e) {
       console.log(e);
     }
-    return cb(data);
+    return data;
+  }).catch(e => {
+      console.log('Error fetching the coin', e);
   });
 }
 
-function fetchData() {
+async function fetchData() {
   console.log('\n');
   console.log(formatDate(new Date()));
-  request('https://api.coinmarketcap.com/v1/global/', (err, res) => {
-    const data = JSON.parse(res.body);
-    const {total_market_cap_usd, total_24h_volume_usd, bitcoin_percentage_of_market_cap} = data;
-    console.log(`MC: ${numeral(total_market_cap_usd).format('($ 0.00 a)')}   Vol: ${numeral(total_24h_volume_usd).format('($ 0.00a)')}   Ratio: ${(total_24h_volume_usd/total_market_cap_usd).toFixed(3)}   BTC Dominance: ${bitcoin_percentage_of_market_cap}%`);
-  });
 
-  URLS.forEach(url => {
-    makeRequest(url, {}, res => {
-      const {name, symbol, rank, price_usd, percent_change_24h } = res
-      const pc = numeral(percent_change_24h).value();
-      const color = pc < 0 ? 'red' : 'green';
-      const pc_text = chalk[color](pc + '%');
-      const price = chalk[color](`$${price_usd}`);
-      const display = `#${rank}\t${name}(${symbol})\t${price}\t${pc_text}`;
-      console.log(display)
-    });
-  });
+  for (let url of URLS) {
+    const coin = await makeRequest(url)
+    console.log(coin)
+  }
 }
 
 
